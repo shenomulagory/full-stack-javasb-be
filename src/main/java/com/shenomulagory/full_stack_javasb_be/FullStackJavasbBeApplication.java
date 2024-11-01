e0789a7613fa55748babad3af5a1df05fbc5abb2
Junaid Muhammad <junaidm.dev@gmail.com>
2024-11-02 00:57:32 +0500

    Java SB Backend Intiating

---
+++ src/main/java/com/shenomulagory/full_stack_javasb_be/FullStackJavasbBeApplication.java
@@ -0,0 +1,13 @@
+package com.shenomulagory.full_stack_javasb_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackJavasbBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackJavasbBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shenomulagory/full_stack_javasb_be/FullStackJavasbBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shenomulagory.full_stack_javasb_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackJavasbBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
